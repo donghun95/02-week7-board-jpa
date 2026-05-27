93715676b9b1fd8e2a44ddb840f84edac9287688
donghun <donghunjeong01@gmail.com>
2026-05-27 23:14:13 +0900

    6주차 ErrorCode 수정 및 requests 내용 수정

--- src/main/java/com/dsa/week5board/common/error/ErrorCode.java
+++ src/main/java/com/dsa/week5board/common/error/ErrorCode.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorCode {
     INVALID_INPUT("E_400_001", HttpStatus.BAD_REQUEST, "입력값이 올바르지 않습니다."),
-    BOARD_NOT_FOUND("E_404_001", HttpStatus.NOT_FOUND, "이미 등록된 게시글입니다."),
+    BOARD_NOT_FOUND("E_404_001", HttpStatus.NOT_FOUND, "게시글을 찾을 수 없습니다."),
     DUPLICATE_BOARD("E_409_001", HttpStatus.CONFLICT, "이미 등록된 게시글입니다."),
     INTERNAL_ERROR("E_500_001", HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다.");
 
